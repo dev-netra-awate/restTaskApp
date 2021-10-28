9180aab1c369e382ef4cc01ba8a4f05d06888a94
dev-netra-awate <92532111+dev-netra-awate@users.noreply.github.com>
2021-10-27 21:14:56 -0400

    assignment-web

---
+++ lab-7-Netra-Awate/app/person.js
@@ -0,0 +1,10 @@
+export class Person{
+    constructor (firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
